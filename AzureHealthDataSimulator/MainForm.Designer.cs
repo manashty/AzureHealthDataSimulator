e82bd672d9242a69e6293614c33d2864126bdbb6
Alireza Manashty <manashty@outlook.com>
2016-08-20 20:30:42 -0300

    Initial Commit

---
+++ AzureHealthDataSimulator/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AzureHealthDataSimulator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_StartSimulation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_StartSimulation
+            // 
+            this.btn_StartSimulation.Location = new System.Drawing.Point(75, 51);
+            this.btn_StartSimulation.Name = "btn_StartSimulation";
+            this.btn_StartSimulation.Size = new System.Drawing.Size(133, 80);
+            this.btn_StartSimulation.TabIndex = 0;
+            this.btn_StartSimulation.Text = "Start Simulation";
+            this.btn_StartSimulation.UseVisualStyleBackColor = true;
+            this.btn_StartSimulation.Click += new System.EventHandler(this.btn_StartSimulation_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_StartSimulation);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_StartSimulation;
+    }
+}
+
---
+++ AzureHealthDataSimulator/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AzureHealthDataSimulator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_StartSimulation_Click(object sender, EventArgs e)
+        {
+            //Populate Values
+            List<DeviceSensors> sensorValues = new List<DeviceSensors>();
+            for (int i = 0; i < 1000; i++)
+            {
+                sensorValues.Add(new DeviceSensors());
+                Debug.WriteLine(sensorValues.Last());
+            }
+        }
+
+        class DeviceSensors
+        {
+            public BloodPressure BP { get; set; } = new BloodPressure();
+            public HeartBeat HB { get; set; } = new HeartBeat();
+
+            public override string ToString()
+            {
+                return BP + ", " + HB;
+            }
+        }
+
+        class BloodPressure
+        {
+            public float Systolic { get; set; }
+            public float Diastolic { get; set; }
+
+            public DateTime Timestamp { get; set; } = DateTime.Now;
+            static Random r = new Random();
+            public BloodPressure()
+            {
+                Systolic = r.Next(9, 15) + (float)Math.Round(r.NextDouble(), 1);
+                Diastolic = r.Next(6, 11) + (float)Math.Round(r.NextDouble(), 1);
+            }
+
+            public override string ToString() { return $"{{Systolic: {Systolic}, Diastolic: {Diastolic} at {Timestamp.ToString()}}}"; }
+
+        }
+
+        class HeartBeat
+        {
+            public short Value { get; set; }
+            //public float Diastolic { get; set; }
+            static Random r = new Random();
+            public DateTime Timestamp { get; set; } = DateTime.Now;
+            public HeartBeat()
+            {
+                Value = (short)r.Next(60, 180); ;
+                //Diastolic = r.Next(9, 15) + (float)Math.Round(r.NextDouble(), 1);
+            }
+            public override string ToString() { return $"{{Heartbeat: {Value} at {Timestamp.ToString()}}}"; }
+        }
+    }
+}
